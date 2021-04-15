115e14623f3017f4896ecca606612277821ed632
David Downie <daviddownie@gmail.com>
2021-04-15 18:20:58 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulSeries {
+        edges {
+          node {
+            series
+          }
+        }
+      }
+    }
+  `)
+
+  const { edges } = data.allContentfulSeries
+
+  return (
+    <div>
+      {edges.map((edge, i) => {
+        const { series } = edge.node
+        return <div key={i}>{series}</div>
+      })}
+    </div>
+  )
 }
